3bb848623680b24911094661f89277825856545d
Keenan Centers <keenan@softwareallies.com>
2013-03-09 14:05:54 -0600

    committing rails files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Onetwentyone::Application.config.secret_token = 'db8354475fe2f7e0cca61aa96ccfcd9cb5c34829a8938996867381c3787e08ce4e63ca348dc342f3fb4a02dc528a152dd935ae831a8118eeaa3573ff31dd985f'
