fd1972a00ce2948e359cac94c00cde0f8b809e4c
Shaun Williams <shaunewilliams@gmail.com>
2013-03-10 15:35:21 -0500

    added route to make home page go to index

--- config/routes.rb
+++ config/routes.rb
@@ -49,7 +49,7 @@ Onetwentyone::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'Onetwentyone#index'
 
   # See how all your routes lay out with "rake routes"
 
